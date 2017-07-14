e974faffd7bd6309b2a909f3a8e921b828b2476c
xiaomlove <i353856593@live.cn>
2017-07-14 19:55:58 +0800

    增强标准化返回函数

--- app/Util/functions.php
+++ app/Util/functions.php
@@ -5,6 +5,20 @@
 
 function normalize($ret = 0, $msg = '', array $data = [])
 {
+    $args = func_get_args();
+	if (is_array($ret))
+    {
+        reset($ret);
+        $ret = current($ret) ? current($ret) : 1;
+        $msg = next($ret);       
+        $data = next($ret);
+    }
+    elseif (func_num_args() < 3 && is_string($args[0]))
+    {
+        $ret = 1;
+        $msg = $args[0];
+        $data = isset($args[1]) ? $args[1] : [];
+    }
     return [
         'ret' => (int)$ret,
         'msg' => (string)$msg,
