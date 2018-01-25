56aeafb7bbbd21851bb31525eddcff23de6d8a36
xiaomiao <i353856593@live.cn>
2018-01-26 02:36:02 +0800

    优化请求OAUth服务器

--- app/Http/Controllers/Api/AuthenticateController.php
+++ app/Http/Controllers/Api/AuthenticateController.php
@@ -25,19 +25,23 @@ class AuthenticateController extends Controller
             'password' => $request->password,
             'scope' => '',
         ];
-        $client = new Client();
+        $url = urlBeforePath() . "/oauth/token";
+        return $this->requestOAuthServer("post", $url, ['form_params' => $data]);
+    }
+
+    private function requestOAuthServer($method, $url, array $options)
+    {
         try
         {
-            $url = urlBeforePath() . "/oauth/token";
-            $response = $client->post($url, ['form_params' => $data]);
+            $client = new Client();
+            $response = call_user_func_array([$client, $method], [$url, $options]);
             $result = json_decode((string)$response->getBody(), true);
             return normalize(0, "OK", $result);
         }
         catch (\Exception $e)
         {
             $result = json_decode((string)$e->getResponse()->getBody(), true);
-            return normalize($result['message'], $request->all());
+            return normalize($result['message'], request()->all());
         }
-
     }
 }
